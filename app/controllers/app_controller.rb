d74894bd964738c756ee445f14a6f631284ac618
dangdunghp96 <dangdunghp59@gmail.com>
2017-11-08 23:01:24 +0700

    week1_fix

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,5 +1,5 @@
 class AppController < ApplicationController
-  skip_before_action :require_signin
+  skip_before_action :require_signin, only: [:index,:mens,:womens]
 
   def index
     @latest_product = Product.order(:updated_at).first 10
@@ -16,10 +16,6 @@ class AppController < ApplicationController
   end
 
   def womens
-
-  end
-
-  def detail
 
   end
 end
