e3478d8b034f70811b2a4a9ce838a7ff05cfb510
Tung Lam <coldwind1911@gmail.com>
2017-11-11 16:13:45 +0700

    Searching function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
-  skip_before_action :require_signin, only: :show
+  skip_before_action :require_signin, only: [:index, :show]
+
+  def index
+    @product = Product.search(params[:search], params[:choice]).paginate(:page => params[:page], :per_page => 16)
+  end
 
   def show
     @product = Product.find_by id: params[:id]
